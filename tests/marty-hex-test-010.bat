ad9f0495263bf8d42005650ba4c48631e6dc505b
Alexandr Martynov <mag@unicont.com>
2025-02-25 17:25:29 +0300

    Добавил десятый тест - вынес заполнение полей в отдельную библиотечную функцию. Готов к генерации HEXа

---
+++ tests/marty-hex-test-010.bat
@@ -0,0 +1,5 @@
+@set TEST_NAME=marty-hex-test-010
+call "%~dp0\.find_test_exe.bat" %TEST_NAME%
+@echo FOUND_EXE: %FOUND_EXE%
+@set TEST_EXE=%FOUND_EXE%
+%TEST_EXE% "%~dp0\layouts\22.txt" "%~dp0\layouts\22_values.txt" > %TEST_NAME%-22.log 2>&1
