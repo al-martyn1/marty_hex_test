0bfb28b18ca8a35118578b99a6058462a385f76d
al-martyn1 <amart@mail.ru>
2025-09-02 17:03:59 +0300

    Присунул marty_containers

--- _libs/_git-job-impl.bat
+++ _libs/_git-job-impl.bat
@@ -1,5 +1,6 @@
 call :DO_GIT_JOB %1 %~dp0\encoding
 call :DO_GIT_JOB %1 %~dp0\marty_bigint
+call :DO_GIT_JOB %1 %~dp0\marty_containers
 call :DO_GIT_JOB %1 %~dp0\marty_cpp
 call :DO_GIT_JOB %1 %~dp0\marty_crc
 call :DO_GIT_JOB %1 %~dp0\marty_csv
